d73659a94fc42d46891a1b2c4403ec1cc825ca6f
KTTRCDL <KTTRCDL@outlook.com>
2024-12-03 18:08:17 +0800

    debug for logging loss error, maybe the issue mentioned in repository cause: model = DDP(model.to(device), device_ids=[device]), not rank

--- train_torchrun_test.sh
+++ train_torchrun_test.sh
@@ -5,5 +5,5 @@
 # torchrun --nnodes=1 --nproc_per_node=4 train_torchrun.py --global-batch-size 256 --model DiT-L/4 --data-path data/imagenet-1k/train
 
 # 4 cards and DiT-B/2
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-torchrun --nnodes=1 --nproc_per_node=8 train_torchrun.py --global-batch-size 256 --model DiT-B/2 --data-path data/imagenet-1k/train
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+torchrun --nnodes=1 --nproc_per_node=4 train_torchrun_test.py --global-batch-size 256 --model DiT-B/2 --data-path data/imagenet-1k/train
